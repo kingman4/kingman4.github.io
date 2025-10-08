3365c6f85ddfd3a40deeb95688fa1d78ade4899c
Ethan King <ethan@Ethans-MacBook-Pro.local>
2025-10-08 14:29:49 -0400

    config

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ export const SITE_DESCRIPTION = 'A simple blog'
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/'
+export const SITE_BASE = ''
 
 export const SocialLinks = [
 	{
